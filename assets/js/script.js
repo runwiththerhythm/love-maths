e5007e7e0f7809616816487625d6e1e7ce4ae251
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-04-10 14:59:28 +0100

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+runGame("addition");
+
 });
 /**
  * The main game 'loop', called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
-    let num1 = Math.floor(Math.random() * 25) + 1;
+function runGame(gameType) {
+    // creates two random number between 1 and 24
+    let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert("Unknown game type: ${gameType}");
+        throw "Unknown game type: ${gaeType}. Aborting!";
+    }
+    
 
 }
 
@@ -42,7 +54,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+
+
 
 }
 
